76059cc25937b5900cd93bf33dcadd96b8c6fb4e
ISMAEL <mugishaismael270@gmail.com>
2026-05-25 15:05:20 +0200

    Create customerRoutes.js

---
+++ src /routes/customerRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/customerController');
+
+router.get('/', controller.getCustomers);
+router.get('/:id', controller.getCustomer);
+router.post('/', controller.createCustomer);
+router.put('/:id', controller.updateCustomer);
+router.delete('/:id', controller.deleteCustomer);
+
+module.exports = router;
